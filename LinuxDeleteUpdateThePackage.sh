e63d0f93609309cf25e3200b9b415cb42215fc2c
mik1th0n <mik1th0n@163.com>
2021-11-21 22:27:22 +0800

    更新Linux删除全部升级包脚本

--- LinuxDeleteUpdateThePackage.sh
+++ LinuxDeleteUpdateThePackage.sh
@@ -1,1 +1,11 @@
-find ./  -name  *.miki  | xargs rm -rf
+find ./  -name  *.bin  | xargs rm -rf
+find ./  -name  *.dat  | xargs rm -rf
+find ./  -name  *.rule  | xargs rm -rf
+find ./  -name  *.av  | xargs rm -rf
+find ./  -name  *.urlibx  | xargs rm -rf
+find ./  -name  *.nti  | xargs rm -rf
+find ./  -name  *.urlib  | xargs rm -rf
+find ./  -name  *.wcs  | xargs rm -rf
+find ./  -name  *.wcl  | xargs rm -rf
+find ./  -name  *.nti  | xargs rm -rf
+find ./  -name  *.geo  | xargs rm -rf
